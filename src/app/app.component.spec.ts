9a55fd76fd2b43aa24dd3dfd68dca8aa1a660a99
Andrej Koller <andrejkoller@outlook.com>
2025-12-01 21:41:38 +0100

    Rename project references from 'quiz-angular' to 'quiztime-frontend' in configuration and test files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'quiz-angular' title`, () => {
+  it(`should have the 'quiztime-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('quiz-angular');
+    expect(app.title).toEqual('quiztime-frontend');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, quiz-angular'
+      'Hello, quiztime-frontend'
     );
   });
 });
